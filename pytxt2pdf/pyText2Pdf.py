948ff2c31f4dbafd235d45b086ec298884189203
Costas Tyfoxylos <costas.tyf@gmail.com>
2014-02-26 17:28:54 +0200

    Corrected the headers for the licence and the author.

--- pytxt2pdf/pyText2Pdf.py
+++ pytxt2pdf/pyText2Pdf.py
@@ -1,27 +1,13 @@
 #!/usr/bin/env python
 #-*- coding: UTF-8 -*-
 # File: pyText2Pdf.py
-# Copyright (c) 2013 by None
 #
-# GNU General Public Licence (GPL)
-# 
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later
-# version.
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-# details.
-# You should have received a copy of the GNU General Public License along with
-# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
-# Place, Suite 330, Boston, MA  02111-1307  USA
-#
-__author__    = 'Costas Tyfoxylos <costas.tyf@gmail.com>'
+# Derived from http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/189858
+
 __docformat__ = 'plaintext'
 __date__      = '04/10/2013'
 
-# Derived from http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/189858
+
 '''
  pyText2Pdf - Python script to convert plain text files into Adobe
  Acrobat PDF files.
